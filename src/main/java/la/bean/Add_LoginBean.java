a9b3e42ee7a8559265319877b933760ed71d0120
WIN-24V478DHT3N\student <2022java42-student006@la-study.com>
2022-05-26 08:48:01 +0900

    komitto

---
+++ src/main/java/la/bean/Add_LoginBean.java
@@ -0,0 +1,14 @@
+package la.bean;
+
+import java.io.Serializable;
+
+public class Add_LoginBean implements Serializable{
+
+	private int login_id;
+	private String password;
+	
+	public Add_Login Bean() {
+		
+		
+	}
+}

032ad32ce3cf0dfa6fe510dccf5a033ab60c6c96
WIN-GG4B8FJLK5R\student <2022java42-student007@la-study.com>
2022-05-27 17:05:05 +0900

    check

---
+++ src/main/java/la/dao/CheckDAO.java
@@ -0,0 +1,14 @@
+package la.dao;
+
+public class CheckDAO {
+	private String url="jdbc:postgresql:webinn";
+	private String user = "webinnuser";
+	private String password = "himitu";
+	
+	public CheckDAO() {
+		
+		
+	}
+	
+
+}
